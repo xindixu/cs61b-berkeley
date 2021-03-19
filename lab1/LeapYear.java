1e8989930a6a008b6d92b2d615d46925b7449e80
Xindi Xu <xindixu@utexas.edu>
2021-03-19 08:08:21 -0500

    lab 1: finish LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,18 @@
  */
 public class LeapYear {
 
+    /** Determine if the year is a leap year
+     */
+    public static boolean isLeapYear(int year){
+        if(year % 400 == 0){
+            return true;
+        }
+        if(year % 4 == 0 && year % 100 != 0){
+            return true;
+        }
+        return false;
+    }
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
