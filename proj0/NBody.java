d3381d62bc48076f3718a6eace8417ece4936b2d
Xindi Xu <xindixu@utexas.edu>
2021-03-25 23:06:39 -0500

    feat(nbody): read data and create planets

---
+++ proj0/NBody.java
@@ -0,0 +1,24 @@
+public class NBody {
+    public static double readRadius(String fileName) {
+        In in = new In(fileName);
+        in.readInt();
+        return in.readDouble();
+    }
+
+    public static Planet[] readPlanets(String fileName) {
+        In in = new In(fileName);
+        int numOfPlanets = in.readInt();
+        in.readDouble();
+        Planet[] planets = new Planet[numOfPlanets];
+        for (int i = 0; i < numOfPlanets; i++) {
+            double xxPos = in.readDouble();
+            double yyPos = in.readDouble();
+            double xxVel = in.readDouble();
+            double yyVel = in.readDouble();
+            double mass = in.readDouble();
+            String imgFileName = in.readString();
+            planets[i] = new Planet(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);
+        }
+        return planets;
+    }
+}
