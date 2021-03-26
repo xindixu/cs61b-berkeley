fdc2cdd877e7b415e159ef4ea98014ddc238c256
Xindi Xu <xindixu@utexas.edu>
2021-03-25 21:58:15 -0500

    feat(planet): set up instance variables and constructors

---
+++ proj0/Planet.java
@@ -0,0 +1,26 @@
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    public Planet(double xxPos, double yyPos, double xxVel, double yyVel, double mass, String imgFileName){
+        this.xxPos = xxPos;
+        this.yyPos = yyPos;
+        this.xxVel = xxVel;
+        this.yyVel = yyVel;
+        this.mass = mass;
+        this.imgFileName = imgFileName;
+    }
+
+    public Planet(Planet p){
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+    }
+}
